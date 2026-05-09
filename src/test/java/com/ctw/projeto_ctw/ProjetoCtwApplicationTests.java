7ab85bd7d1670cabf7b26ba9cfa67582614fbdfe
Art_Mainardi <iensenmainardi@gmail.com>
2026-05-08 21:37:29 -0300

    Initial Commit

---
+++ src/test/java/com/ctw/projeto_ctw/ProjetoCtwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctw.projeto_ctw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCtwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
