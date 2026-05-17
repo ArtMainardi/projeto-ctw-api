41afe0cffb1840061670cb06980d60232672973b
Art_Mainardi <iensenmainardi@gmail.com>
2026-05-17 11:16:41 -0300

    Criado FOREIGN KEY entre as tabelas ALUNOS e TURMAS

--- src/main/java/com/ctw/projeto_ctw/turmas/model/Turmas.java
+++ src/main/java/com/ctw/projeto_ctw/turmas/model/Turmas.java
@@ -1,8 +1,11 @@
 package com.ctw.projeto_ctw.turmas.model;
+
+import com.ctw.projeto_ctw.alunos.model.Alunos;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Entity
 @Table(name = "turmas")
@@ -20,4 +23,7 @@ public class Turmas {
     @ManyToOne
     @JoinColumn(name = "periodos_id_periodo")
     private Long id_periodo;
+    // --
+    @OneToMany(mappedBy = "turmas")
+    List<Alunos> alunos;
 }
