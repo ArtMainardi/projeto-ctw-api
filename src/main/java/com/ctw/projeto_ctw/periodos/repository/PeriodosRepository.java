6b405b078de87fa08d4e3e7d0558732ae3e2dda9
Art_Mainardi <iensenmainardi@gmail.com>
2026-05-17 10:54:36 -0300

    Criação do REPOSITORY da tabela PERIODOS

---
+++ src/main/java/com/ctw/projeto_ctw/periodos/repository/PeriodosRepository.java
@@ -0,0 +1,7 @@
+package com.ctw.projeto_ctw.periodos.repository;
+
+import com.ctw.projeto_ctw.periodos.model.Periodos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PeriodosRepository extends JpaRepository<Periodos, Long> {
+}
