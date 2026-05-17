d67f2828da8ccd148ec21083a58c6bd0457bef41
Art_Mainardi <iensenmainardi@gmail.com>
2026-05-17 08:38:08 -0300

    Criação do REPOSITORY da tabela TURMAS

---
+++ src/main/java/com/ctw/projeto_ctw/turmas/repository/TurmasRepository.java
@@ -0,0 +1,7 @@
+package com.ctw.projeto_ctw.turmas.repository;
+
+import com.ctw.projeto_ctw.turmas.model.TurmasModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurmasRepository extends JpaRepository<TurmasModel, Long> {
+}
