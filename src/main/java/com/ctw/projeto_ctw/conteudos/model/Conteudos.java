1b2942fa8cd9cd89fb0c7cdef33c2511eae9ce1f
Art_Mainardi <iensenmainardi@gmail.com>
2026-05-18 18:18:52 -0300

    Criação do MODEL da tabela CONTEUDOS

---
+++ src/main/java/com/ctw/projeto_ctw/conteudos/model/Conteudos.java
@@ -0,0 +1,30 @@
+package com.ctw.projeto_ctw.conteudos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "conteudos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Conteudos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_conteudo;
+    // --
+    @Column(length = 40)
+    private String titulo_conteudo;
+    // --
+    @Column(columnDefinition = "TEXT")
+    private String descricao_conteudo;
+    // --
+    private LocalDate data_publicacao;
+    //--
+    // --> Futura relação com MATERIAS
+}
