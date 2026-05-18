101fbfcd97fa286a6d14785b1ade03bf229e3a4f
Matheus-Lacerda009 <matheuslacerda.mala@gmail.com>
2026-05-17 22:57:25 -0300

    Criando o service e o controller

--- src/main/java/com/example/demo/controller/ProfessorController.java
+++ src/main/java/com/example/demo/controller/ProfessorController.java
@@ -1,4 +1,44 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Materia;
+import com.example.demo.model.Professor;
+import com.example.demo.service.MateriaService;
+import com.example.demo.service.ProfessorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/professor")
+@RequiredArgsConstructor
 public class ProfessorController {
+    private final ProfessorService service;
+
+    @GetMapping
+    public List<Professor> listar(){
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Professor> buscar(@PathVariable Long id){
+        return ResponseEntity.ok(service.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Professor> salvar(@RequestBody Professor prof){
+        return ResponseEntity.ok(service.salvar(prof));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Professor> atualizar(@PathVariable Long id, @RequestBody Professor prof){
+        return ResponseEntity.ok(service.atualizar(id, prof));
+    }
+
+    @DeleteMapping("/{id^}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        service.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/example/demo/service/MateriaService.java
+++ src/main/java/com/example/demo/service/MateriaService.java
@@ -1,5 +1,33 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Materia;
+import com.example.demo.repository.MateriaRepository;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MateriaService {
+    private final MateriaRepository repository;
+
+    public List<Materia> listar(){
+        return repository.findAll();
+    }
+    public Materia buscarPorId(Long id){
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Matéria não encontrada!"));
+    }
+    public Materia salvar(Materia mat){
+        return repository.save(mat);
+    }
+    public Materia atualizar(Long id, Materia mat){
+        Materia mod = buscarPorId(id);
+        mod.setNome_materia(mat.getNome_materia());
+        mod.setProfessores(mat.getProfessores());
+        return repository.save(mod);
+    }
+    public void excluir(Long id){
+        repository.deleteById(id);
+    }
 }
