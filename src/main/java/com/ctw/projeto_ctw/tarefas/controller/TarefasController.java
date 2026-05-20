99867e95454389cb477f150f00efba59803f97e2
Art_Mainardi <iensenmainardi@gmail.com>
2026-05-20 16:49:43 -0300

    Criação do CONTROLLER da tabela TAREFAS

---
+++ src/main/java/com/ctw/projeto_ctw/tarefas/controller/TarefasController.java
@@ -0,0 +1,42 @@
+package com.ctw.projeto_ctw.tarefas.controller;
+
+import com.ctw.projeto_ctw.tarefas.model.Tarefas;
+import com.ctw.projeto_ctw.tarefas.service.TarefasService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tarefas")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+
+public class TarefasController {
+    private final TarefasService service;
+
+    @GetMapping
+    public List<Tarefas> listar(){ return service.listar(); }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tarefas> buscar(@PathVariable Long id){
+        return ResponseEntity.ok(service.buscar(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Tarefas> salvar(@RequestBody Tarefas newTarefa){
+        return ResponseEntity.ok(service.salvar(newTarefa));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Tarefas> modificar(@RequestBody Tarefas modifiedTarefa, @PathVariable Long id){
+        return ResponseEntity.ok(service.modificar(modifiedTarefa, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
