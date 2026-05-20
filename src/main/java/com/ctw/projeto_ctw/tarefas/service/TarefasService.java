17c332c3bf38a070e8d1f58ec8f8dffeef492e61
Art_Mainardi <iensenmainardi@gmail.com>
2026-05-20 16:36:00 -0300

    Criação do SERVICE da tabela TAREFAS

---
+++ src/main/java/com/ctw/projeto_ctw/tarefas/service/TarefasService.java
@@ -0,0 +1,38 @@
+package com.ctw.projeto_ctw.tarefas.service;
+
+import com.ctw.projeto_ctw.tarefas.model.Tarefas;
+import com.ctw.projeto_ctw.tarefas.repository.TarefasRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+
+public class TarefasService {
+    private final TarefasRepository repository;
+
+    // Listar todos os dados:
+    public List<Tarefas> listar(){ return repository.findAll(); }
+
+    // Buscar por id:
+    public Tarefas buscar(Long id){
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Tarefa não encontrada!"));
+    }
+
+    // Salvar dado:
+    public Tarefas salvar(Tarefas newTarefa) { return repository.save(newTarefa); }
+
+    // Modificar um dado:
+    public Tarefas modificar(Tarefas modifiedTarefa, Long id){
+        Tarefas newTarefa = buscar(id);
+        newTarefa.setTitulo_tarefa(modifiedTarefa.getTitulo_tarefa());
+        newTarefa.setTexto_tarefa(modifiedTarefa.getTexto_tarefa());
+        newTarefa.setData_publicacao_tarefa(modifiedTarefa.getData_publicacao_tarefa());
+        newTarefa.setData_entrega_tarefa(modifiedTarefa.getData_entrega_tarefa());
+        return repository.save(newTarefa);
+    }
+
+    // Deletar um dado:
+    public void deletar(Long id){ repository.deleteById(id); }
+}
