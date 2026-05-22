c36bf961adc5378bf38c30224e6fff6f6181e5dd
Art_Mainardi <iensenmainardi@gmail.com>
2026-05-22 20:16:00 -0300

    Correções finais: @CrossOrigin e server.port

--- src/main/java/com/ctw/projeto_ctw/conteudos/controller/ConteudosController.java
+++ src/main/java/com/ctw/projeto_ctw/conteudos/controller/ConteudosController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/conteudos")
 @RequiredArgsConstructor
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 
 public class ConteudosController {
     private final ConteudosService service;
--- src/main/java/com/ctw/projeto_ctw/tarefas/controller/TarefasController.java
+++ src/main/java/com/ctw/projeto_ctw/tarefas/controller/TarefasController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/tarefas")
 @RequiredArgsConstructor
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 
 public class TarefasController {
     private final TarefasService service;
